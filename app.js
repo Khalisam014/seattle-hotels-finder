228871b97f8dd54b6aa0aabc321d3289c36a2d44
khalisam <khalisam@uw.edu>
2024-06-04 01:39:12 -0700

    app linter changes

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ const ONE_THOUSAND = 1000;
 
 const CLIENT_ERROR = 400;
 const SERVER_ERROR = 500;
+const FOUR_O_FOUR = 404;
 const SERVER_ERROR_MSG = 'An error occurred on the server. Try again later.';
 
 app.get('/hotels', async (req, res) => {
@@ -88,19 +89,19 @@ app.post('/account/create', async (req, res) => {
     let name = req.body.name;
     let email = req.body.email;
     let password = req.body.password;
-    let phone_number = req.body.phone_number;
+    let phoneNumber = req.body.phoneNumber;
     let address = req.body.address;
 
-    if (username && name && email && password && phone_number && address) {
+    if (username && name && email && password && phoneNumber && address) {
       let db = await getDBConnection();
       let prevUsername = await db.get('SELECT * FROM users WHERE username = ?', username);
       if (prevUsername) {
-        return res.status(400).send('Username already exists');
+        return res.status(CLIENT_ERROR).send('Username already exists');
       }
       if (!prevUsername) {
         let query = 'INSERT INTO users (username, name, email, password, phone_number, address) ' +
           'VALUES (?,?,?,?,?,?)';
-        await db.run(query, username, name, email, password, phone_number, address);
+        await db.run(query, username, name, email, password, phoneNumber, address);
         res.json({'username': username});
        } else {
          res.status(CLIENT_ERROR).type('text')
@@ -155,20 +156,20 @@ app.get('/transaction', async (req, res) => {
 
 app.post('/reserve', async (req, res) => {
   try {
-    let user_id = req.body.user_id;
+    let userId = req.body.userId;
     let roomId = req.body.roomId;
     let checkInDate = req.body.checkInDate;
     let checkOutDate = req.body.checkOutDate;
 
-    if (user_id && roomId && checkInDate && checkOutDate) {
+    if (userId && roomId && checkInDate && checkOutDate) {
       let db = await getDBConnection();
-      let errorText = await isReservationValid(db, user_id, roomId, checkInDate, checkOutDate);
+      let errorText = await isReservationValid(db, userId, roomId, checkInDate, checkOutDate);
       if (errorText === '') {
-        let total_price = await findTotalPrice(db, roomId, checkInDate, checkOutDate);
+        let totalPrice = await findTotalPrice(db, roomId, checkInDate, checkOutDate);
         let query = 'INSERT INTO reservations (user_id, room_id, checkInDate, check_out_date,' +
           'total_price) VALUES (?,?,?,?,?)';
-        let result = await db.run(query, user_id, roomId, checkInDate, checkOutDate, total_price);
-        res.json({'reservation_id': result.lastID, 'total_price': total_price});
+        let result = await db.run(query, userId, roomId, checkInDate, checkOutDate, totalPrice);
+        res.json({'reservation_id': result.lastID, 'total_price': totalPrice});
       } else {
         res.status(CLIENT_ERROR).type('text')
           .send(errorText);
@@ -193,7 +194,7 @@ app.get('/account/:username', async (req, res) => {
     let query = 'SELECT * FROM users WHERE username = ?';
     let userResult = await db.get(query, username);
     if (!userResult) {
-      res.status(404).send('User not found');
+      res.status(FOUR_O_FOUR).send('User not found');
       return;
     }
 
@@ -201,7 +202,7 @@ app.get('/account/:username', async (req, res) => {
       username: userResult.username,
       email: userResult.email,
       name: userResult.name,
-      phone_number: userResult.phone_number,
+      phoneNumber: userResult.phoneNumber,
       address: userResult.address
     };
 
@@ -209,10 +210,9 @@ app.get('/account/:username', async (req, res) => {
     await db.close();
   } catch (err) {
     console.error(err);
-    res.status(500).send('An error occurred while fetching user data');
+    res.status(SERVER_ERROR).send('An error occurred while fetching user data');
   }
 });
-
 
 /**
  * A helper function for the '/hotels' endpoint. Filters based on the parameters.
@@ -269,10 +269,10 @@ async function organizeHotelData(db, hotels, amenity, checkIn, checkOut) {
         'address': element.address,
         'description': element.description,
         'rating': element.rating,
-        'phone_number': element.phone_number,
+        'phone_number': element.phoneNumber,
         'amenities': amenityNames,
         'rooms': availableRooms
-      }
+      };
       result.hotels.push(hotel);
     }
   }
@@ -285,15 +285,15 @@ async function organizeHotelData(db, hotels, amenity, checkIn, checkOut) {
  * @returns {JSON} - the reformatted JSON
  */
 function formatUserTransactionData(result) {
-  let userTransactions = {'user_id': result[0].user_id, 'reservations': []};
+  let userTransactions = {'user_id': result[0].userId, 'reservations': []};
   for (const element of result) {
     let reservation = {
       'reservation_id': element.reservation_id,
       'room_id': element.roomId,
       'check_in_date': element.checkInDate,
       'check_out_date': element.checkOutDate,
-      'total_price': element.total_price
-    }
+      'total_price': element.totalPrice
+    };
     userTransactions['reservations'].push(reservation);
   }
   return userTransactions;
@@ -304,13 +304,13 @@ function formatUserTransactionData(result) {
  * overlapping reservations. Returns the error message if needed, otherwise
  * empty string.
  * @param {Object} db - the database object for the connection
- * @param {Number} user_id - the user's id
+ * @param {Number} userId - the user's id
  * @param {Number} roomId - the id of the room that is trying to be reserved.
  * @param {Date} checkInDate - the check in date trying to be reserved
  * @param {Date} checkOutDate - the check out date trying to be reserved
  * @returns {String} - the error message, otherwise empty string.
  */
-async function isReservationValid(db, user_id, roomId, checkInDate, checkOutDate) {
+async function isReservationValid(db, userId, roomId, checkInDate, checkOutDate) {
   let roomExists = await db.get('SELECT * FROM rooms WHERE room_id = ?', roomId);
   if (!roomExists) {
     return 'This room does not exist.';
@@ -325,15 +325,26 @@ async function isReservationValid(db, user_id, roomId, checkInDate, checkOutDate
       (check_in_date <= ? AND check_out_date >= ?) OR
       (check_in_date < ? AND check_out_date >= ?)
     )`;
-  let roomAvailable = await db.get(roomQuery, roomId, checkInDat, checkOutDate,
-    checkInDat, checkOutDate, checkInDat, checkInDat, checkOutDate, checkOutDate);
+  let roomAvailable = await db.get
+  (
+    roomQuery,
+    roomId,
+    checkInDate,
+    checkOutDate,
+    checkInDate,
+    checkOutDate,
+    checkInDate,
+    checkInDate,
+    checkOutDate,
+    checkOutDate
+  );
   if (roomAvailable) {
     return 'This room is already reserved';
   }
 
-  if (user_id != undefined) {
+  if (userId !== undefined) {
     let userQuery = 'SELECT check_in_date, check_out_date FROM reservations WHERE user_id = ?';
-    let results = await db.all(userQuery, user_id);
+    let results = await db.all(userQuery, userId);
 
     for (let element of results) {
       if ((element.checkInDate <= checkOutDate && element.checkOutDate >= checkInDate) ||
